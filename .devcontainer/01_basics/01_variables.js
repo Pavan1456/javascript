719538db8508b6c77ebb01859a227d19a5e9e2da
Pavan agrawal <142879502+Pavan1456@users.noreply.github.com>
2024-10-22 16:41:17 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553;
+let accountEmail = "abcd@gmail.com";
+var accountPassword = "12345";
+accountCity = "Mathura";
+let accountstate;
+
+// accountId = 2
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello");
+// alert(3+2); // not available here in node js
+console.log(4+4);
+console.log("agrawal");
+
+let name ="pawan";
+let age = 18;
+let isLoggedIn = false;
+
+// number , bigint , string , boolean ,null (standalone value)
